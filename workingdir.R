29278463d9d1d88a0389b7b23fce933e86457492
Joe Westenberg <jwesten@iu.edu>
2021-02-17 16:23:17 -0500

    added files from 20210217

---
+++ workingdir.R
@@ -0,0 +1,7 @@
+#workingdir<-"C:/Users/weste/Documents/GitHub/Rcourse_Monday/"
+
+folder_figures<-paste0(workingdir, "figures")
+folder_processed_data<-paste0(workingdir, "processed_data")
+folder_raw_data<-paste0(workingdir, "raw_data")
+folder_scripts<-paste0(workingdir, "scripts")
+folder_tables<-paste0(workingdir, "tables")
